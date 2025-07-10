230e17922b9e920684f236c18c2448b4aba0fc59
Dayo <DayoTitiloye@Outlook.Com>
2025-07-10 15:37:48 +0100

    Finished Routes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+#register view
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -47,6 +48,7 @@ def register():
 
     return render_template('auth/register.html')
 
+# Login view
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -71,3 +73,31 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+# Created view to store logged in user
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+# Checks if user is logged in, if not return to login page
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
